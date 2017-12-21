1b48fcc3704adcb59a19a74bb5117a61136c8307
Andrew Nicholson <Andrew.Nicholson@ithaka.org>
2017-12-21 13:08:21 -0500

    day one captcha puzzle

--- src/main/java/org/wandotini/DayOneCaptcha.java
+++ src/main/java/org/wandotini/DayOneCaptcha.java
@@ -2,9 +2,29 @@ package org.wandotini;
 
 public class DayOneCaptcha {
 
+    private static final String DIGITS = "\\d+";
+
     public static int parse(String s) {
+        validateInput(s);
+        if (s.length() == 1)
+            return 0;
+        return sumMatchingDigits(s);
+    }
+
+    private static int sumMatchingDigits(String s) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int nextChar = i == s.length() - 1 ? 0 : i + 1;
+            if (s.charAt(i) == s.charAt(nextChar))
+                sum += Integer.parseInt(s.substring(nextChar, nextChar + 1));
+        }
+        return sum;
+    }
+
+    private static void validateInput(String s) {
         if (s == null)
             throw new IllegalArgumentException("input must be non-null");
-        return 0;
+        if (!s.matches(DIGITS))
+            throw new IllegalArgumentException("input must be a string of integer digits");
     }
 }
