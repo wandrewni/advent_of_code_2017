c693ccb94186e5083de604abccaad2a2b5565434
Andrew Nicholson <Andrew.Nicholson@ithaka.org>
2017-12-21 12:42:15 -0500

    initial commit-ish

---
+++ src/main/java/org/wandotini/DayOneCaptcha.java
@@ -0,0 +1,10 @@
+package org.wandotini;
+
+public class DayOneCaptcha {
+
+    public static int parse(String s) {
+        if (s == null)
+            throw new IllegalArgumentException("input must be non-null");
+        return 0;
+    }
+}
