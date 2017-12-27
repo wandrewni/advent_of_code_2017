9f7bee7354a6ce78c13461e9f609008a7ea1040a
Andrew Nicholson <Andrew.Nicholson@ithaka.org>
2017-12-27 16:12:51 -0500

    day five part two done

---
+++ src/main/java/org/wandotini/DayFourTrampolineJumpsPartTwo.java
@@ -0,0 +1,18 @@
+package org.wandotini;
+
+public class DayFourTrampolineJumpsPartTwo {
+    public static int navigate(Integer[] jumps) {
+        int offset = 0;
+        int steps = 0;
+        while (offset < jumps.length && offset > -1) {
+            steps++;
+            Integer foundOffset = jumps[offset];
+            if (foundOffset > 2)
+                jumps[offset]--;
+            else
+                jumps[offset]++;
+            offset += foundOffset;
+        }
+        return steps;
+    }
+}
---
+++ src/test/java/org/wandotini/DayFourTrampolineJumpsPartTwoTest.java
@@ -0,0 +1,62 @@
+package org.wandotini;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class DayFourTrampolineJumpsPartTwoTest {
+
+    @Test
+    public void emptyPathReturns0() throws Exception {
+        verifyNavigate(0);
+    }
+
+    @Test
+    public void singleOneReturnsOne() throws Exception {
+        verifyNavigate(1, 1);
+    }
+
+    @Test
+    public void TwoOnesReturnsTwo() throws Exception {
+        verifyNavigate(2, 1, 1);
+    }
+
+    @Test
+    public void TwoLengthStartingWithTwoReturnsOne() throws Exception {
+        verifyNavigate(1, 2, 1);
+    }
+
+    @Test
+    public void OneLengthWithNegativeOneReturnsZero() throws Exception {
+        verifyNavigate(1, -1);
+    }
+
+    @Test
+    public void vistingASpotIncreasesItsOffset() throws Exception {
+        verifyNavigate(2, 0);
+        verifyNavigate(4, 0, -1);
+        verifyNavigate(10, 0, 3, 0, 1, -3);
+    }
+
+    @Test
+    public void puzzleInput() throws Exception {
+        InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream("day_five_input.txt");
+        Integer[] jumps = new BufferedReader(new InputStreamReader(resourceAsStream))
+                .lines()
+                .map(Integer::parseInt)
+                .collect(toList())
+                .toArray(new Integer[0]);
+
+        verifyNavigate(29629538, jumps);
+    }
+
+    private void verifyNavigate(int expectedSteps, Integer... jumps) {
+        assertThat(DayFourTrampolineJumpsPartTwo.navigate(jumps), is(expectedSteps));
+    }
+}
